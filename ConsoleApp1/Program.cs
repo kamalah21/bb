ce8231753674c5dc354dbb4e802d11d3847b96a0
Kamran <kamalah@code.edu.az>
2024-04-20 19:27:39 +0400

    ConsoleApp1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,500 @@
+﻿#region # 1
+//Console.WriteLine("bir eded daxil edin");
+//int number = Convert.ToInt32(Console.ReadLine());
+//if (number > 0)
+
+//{
+//    Console.WriteLine("daxil edilen eded musbetdir");
+//}
+//else
+//{
+//    Console.WriteLine("daxil edilen eded menfidir");
+//}
+#endregion 
+#region # 2
+//Console.WriteLine("number1 ededini daxil et");
+//int number1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("number2 ededini daxil et");
+//int number2 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("number3 ededini daxil et");
+//int number3 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("4 ededini daxil et");
+//int number4 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("number5 ededini daxil et");
+//int number5 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine(number1 + number2 + number3 + number4 + number5);
+#endregion 
+#region # 3
+//Console.ReadLine("1 eded daxil edin");
+//int number = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine(Math.Pow(number, 2));
+#endregion
+#region # 4
+//Console.WriteLine("number1 ededi daxil edin");
+//int number1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("number2 ededi daxil edin");
+//int number2 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("number3 ededi daxil edin");
+//int number3 = Convert.ToInt32(Console.ReadLine());
+//if (number1 > number2 && number2 > number3)
+//{
+//    Console.WriteLine( number1 + " en boyuk ededdir");
+
+//}
+//else if (number1 < number2 && number2 < number3)
+//{
+//    Console.WriteLine(number3 + "en boyuk ededdir");
+//}
+//else
+//{
+//    Console.WriteLine(number2 + "en boyuk ededdir");    
+//}
+#endregion
+#region # 5
+//Console.WriteLine("1-ci ededi daxil edin");
+//int number1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("2-ci ededi daxil edin");
+//int number2 = Convert.ToInt32(Console.ReadLine());
+//if (Math.Pow(number1, 3) > number2)
+//{
+//    Console.WriteLine("1-ci ededin kubu 2-ci ededden boyukdur");
+//}
+//else
+//{
+//    Console.WriteLine("1-ci ededin kubu 2-ci ededden kicikdir");
+//}
+#endregion
+#region # 6
+//Console.WriteLine("Eded daxil et");
+//int number = Convert.ToInt32(Console.ReadLine());
+//if (number % 3 == 0 && number % 5 == 0)
+//{
+//    Console.WriteLine("eded hem 3-e hem 5-e bolunur");
+
+//}
+//else
+//{
+//    Console.WriteLine("eded hem 3-e hem 5-e bolunmur");
+//}
+#endregion
+#region # 7
+//Console.WriteLine("kartdaki meblegi daxil edin");
+//int number1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("mehsulun qiymetini daxil edin");
+//int number2 = Convert.ToInt32(Console.ReadLine());
+//if (number1 > number2)
+//{
+//    Console.WriteLine("Kartin meblegi mehsulu ala biler");
+//}
+//else
+//{
+
+//    Console.WriteLine("Kartin meblei mehsulu ala bilmir");
+//}
+#endregion
+#region # 8
+//Console.WriteLine("1-ci fenni daxil edin");
+//int number1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("2-ci fenni daxil edin");
+//int number2 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("3-cu fenni daxil edin");
+//int number3 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("4-ci fenni daxil edin");
+//int number4 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("5-ci fenni daxil edin");
+//int number5 =  Convert.ToInt32(Console.ReadLine());
+//int ortalama = (number1 + number2 + number3 + number4 + number5) / 5;
+//if (ortalama >= 65 && ortalama <= 100)
+//{
+//    Console.WriteLine("Semestri kecib");
+//}
+//else
+//{
+//    Console.WriteLine("Semestri kecmiyib");
+//}
+#endregion # #
+#region # 9
+//Console.WriteLine("1-ci ededi daxil edin");
+//int number1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("2-ci ededi daxil edin");
+//int number2 = Convert.ToInt32(Console.ReadLine());
+//if (number1 > number2)
+//{
+//    Console.WriteLine("1-ci eded 2-cinden boyukdur");
+//}
+//else
+//{
+//    Console.WriteLine("1-ci eded 2-cinden kicikdir");
+//}
+#endregion
+#region # 10
+//Console.WriteLine("suyun  tempraturu");
+//int number = Convert.ToInt32(Console.ReadLine());
+
+//if (number>0 )
+//{
+//    Console.WriteLine("Suyun donma tempraturundan yuxaridir");
+//}
+//else if (number==0)
+//{
+//    Console.WriteLine("Suyun donma  tempraturuna beraberdir");
+//}
+//else
+//{
+//    Console.WriteLine("Suyun donma tempraturundan asagidir");
+//}
+#endregion
+#region # 1
+//for (int i = 0; i <= 50; i++)
+//{
+//    Console.WriteLine(i);
+//}
+#endregion
+#region # 2
+//int count = 0;
+//for (int i = 0; i < 40; i++)
+//{
+//    if (i % 3 == 0)
+//        count++;
+//}
+
+//{
+//Console.WriteLine(count);
+//}
+#endregion
+#region # 3
+//for (int i = 0; i < 100; i++) 
+//{
+//    if (i % 3 == 0 && i % 7 == 0)
+//    {
+//        Console.WriteLine(i);
+//    }
+//}
+#endregion
+#region # 4
+//Console.WriteLine("Eded daxil et");
+//int number = Convert.ToInt32(Console.ReadLine());
+//int digit_count = 0;
+//int TempNumber = number;
+//if (number == 0)
+//{
+//    digit_count = 1;
+//}
+//else if (number > 0)
+//{
+//while (TempNumber != 0)
+//{
+//    TempNumber /= 10;
+//    digit_count++;
+//}
+//Console.WriteLine(digit_count);
+//}
+#endregion
+#region # 5
+
+//Console.WriteLine("Ededi daxil edin");
+//int number = Convert.ToInt32(Console.ReadLine());
+//int temp = 0;
+//while (number > 0)
+//{
+//    temp = (temp * 10) + (number % 10);
+//    number = number / 10;
+//}
+//Console.WriteLine(temp);
+#endregion
+#region # 6
+//Console.WriteLine("eded daxil edin");
+//int number = Convert.ToInt32(Console.ReadLine());
+//bool s = true;
+//for (int i = 2;  i < (int)Math.Sqrt(number); i++)
+//{
+//    if (number % i == 0)
+//    {
+//        s = false;
+//        break;
+//    }
+//}
+//if (s)
+//{
+//    Console.WriteLine("eded sadedir");
+//}
+//else
+//{
+//    Console.WriteLine("eded murekkebdir");
+//}
+#endregion
+#region # 7
+//Console.WriteLine("Eded daxil edin");
+//int number = Convert.ToInt32(Console.ReadLine());
+//int sum = 0;
+//while (number != 0)
+//{
+//    sum += number % 10;
+//    number = number / 10;
+//}
+//Console.WriteLine(sum);
+#endregion
+#region # 8
+//Console.WriteLine("eded daxil edin");
+//int number = Convert.ToInt32(Console.ReadLine());
+//int max = 0;
+//while (number > 0)
+//{
+//    if (max < number % 10) max = number % 10;
+//    number /= 10;
+//}
+//Console.WriteLine(max);
+#endregion
+#region # 9
+//for (int i = 1; i <= 70; i++)
+//{
+//    if (i % 2 == 0)
+//    {
+//        Console.WriteLine(i);
+//    }
+//}
+#endregion
+#region # 10
+//Console.WriteLine("ay daxil et");
+//int number = Convert.ToInt32(Console.ReadLine());
+//switch (number)
+//{
+//    case 1:
+//    case 2:
+//    case 12:
+//        Console.WriteLine("qis");
+//        break;
+//    case 3:
+//    case 4:
+//    case 5:
+//        Console.WriteLine("yaz");
+//        break;
+//    case 6:
+//    case 7:
+//    case 8:
+//        Console.WriteLine("yay");
+//        break;
+//    case 9:
+//    case 10:
+//    case 11:
+//        Console.WriteLine("payiz");
+//        break;
+//    default:
+//        Console.WriteLine("sehv reqem girilib");
+//        break;
+//}
+#endregion
+#region  #11
+//Console.Write("input size");
+//int number = Convert.ToInt32(Console.ReadLine());
+//for (int i = 0; i < number; i++)
+//{
+//    Console.Write("*");
+//}
+//Console.WriteLine();
+//for (int i = 0; i < number - 2; i++)
+//{
+//    Console.Write("*");
+//}
+//for (int j = 0; j < number - 2; j++)
+//{
+//    Console.Write("*");
+//}
+//for (int i = 0; i < number; i++)
+//{
+//    Console.Write("*");
+//}
+//Console.WriteLine();
+#endregion
+#region # 12
+//Console.Write("input size");
+//int number = Convert.ToInt32(Console.ReadLine());
+//for (int i = 1; i < number; i++)
+//{
+//    for (int j = 1; j <= number; j++)
+//    {
+//        Console.Write("*");
+//    }
+//}
+//Console.WriteLine();
+#endregion
+#region    # 13
+//Console.Write("input size");
+//int number = Convert.ToInt32(Console.ReadLine());
+//for (int j = number; j >= 1; j--)
+//{
+//    for (int i = 1; i <= j; i++)
+//    {
+//        Console.Write("*");
+//    }
+//}
+//Console.WriteLine();
+#endregion
+
+
+
+#region #1
+//int number   = 10;
+//int[] numbers = { 100, 200, 300, 400 };
+
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    Console.WriteLine(numbers[i]);  
+//}
+#endregion
+#region #2
+//int[] numbers = { 100, 200, 300 };
+//int sum = 0;
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    sum += numbers[i];
+//};
+//Console.WriteLine(sum);
+#endregion
+#region # 3 
+//int[] numbers = { 100, 200, 300 };
+//for (int i = numbers.Length - 1; i >= 0; i--)
+//{
+//    Console.WriteLine(numbers[i]);
+//}
+#endregion
+#region # 4
+//int[] numbers = { 100, 200, 300 };
+//for (int i = 0; i < numbers.Length / 2; i++)
+//{
+//    int temp = numbers[i];
+//    numbers[i] = numbers[numbers.Length - i - 1];
+//    numbers[numbers.Length - i - 1] = temp;
+//}
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    Console.WriteLine(numbers[i]);
+//}
+#endregion
+#region # 5 
+
+//int max = 0;
+//int[] numbers = { 100, 200, 300 };
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//        if (numbers[i] > max)
+//          max = numbers[i];
+//}
+//Console.WriteLine(max);
+#endregion
+#region # 6
+//int count = 0;
+//int[] numbers = { 100, 200, 300 };
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    count++;
+//}
+//Console.WriteLine(count);
+#endregion
+#region # 7
+//selection Sort
+
+//int[] arr = { 1, 2, 3, 4,34,1,765,12,76, 5, 6 };
+//int n = arr.Length; 
+//Console.WriteLine("Selection sort");
+//Console.Write("Initial array is: ");
+//for (int i = 0; i < n; i++)
+//{
+//    Console.Write(arr[i] + " ");
+//}
+//int temp, smallest;
+//for (int i = 0; i < n - 1; i++)
+//{
+//    smallest = i;
+//    for (int j = i + 1; j < n; j++)
+//    {
+//        if (arr[j] < arr[smallest])
+//        {
+//            smallest = j;
+//        }
+//    }
+//    temp = arr[smallest];
+//    arr[smallest] = arr[i];
+//    arr[i] = temp;
+//}
+//Console.WriteLine();
+//Console.Write("Sorted array is: ");
+//for (int i = 0; i < n; i++)
+//{
+//    Console.Write(arr[i] + " ");
+//}
+#endregion
+#region # 8
+//int count = 0 ;
+//int[] numbers = { 100, 200, 300, 400 };
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    if (numbers[i] == 0)
+//    {
+//        count++;
+//    }
+//}
+//Console.WriteLine(count);
+#endregion
+#region # 9
+//int count = 0;
+//int[] number = { 1, 2, 3, 4, 5, 7, 9 };
+//for (int i = 0; i < number.Length; i++)
+//{
+//    if (number[i] < 0)
+//    {
+//        count++;
+//    }
+
+//}
+//Console.WriteLine(count);
+#endregion
+#region # 10
+//int sum = 0;
+//int[] number = { 1, 2, 3, 4, 5, 6, 7 };
+//for (int i = 0; i < number.Length; i++)
+//{
+//    if (number[i] > 0)
+//    {
+//        sum+= number[i];
+//    }
+//}
+//Console.WriteLine(sum);
+#endregion
+#region # 11
+//int[] number = { 1, 2, 4, 5 };
+//for (int i = 0; i <number.Length; i++)
+//{
+//    Console.WriteLine(number[i] * number[i]);
+//}
+#endregion
+#region # 12
+//int[] arr = { 56, 1, 99, 67, 89, 23, 44, 12, 78, 34 };
+//int n = arr.Length; ;
+
+//int  smallest;
+//for (int i = 0; i < n - 1; i++)
+//{
+//    smallest = i;
+//    for (int j = i + 1; j < n; j++)
+//    {
+//        if (arr[j] < arr[smallest])
+//        {
+//            smallest = j;
+//        }
+//    }
+//    int temp = arr[smallest];
+//    arr[smallest] = arr[i];
+//    arr[i] = temp;
+//}
+//for (int i = 0; i < arr.Length / 2; i++)
+//{
+//    int temp = arr[i];
+//    arr[i] = arr[arr.Length - i - 1];
+//    arr[arr.Length - i - 1] = temp;
+//}
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    Console.WriteLine(arr[i]);
+//}
+#endregion
